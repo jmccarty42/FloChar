83695b7ce3df0aacc20b83ffa9d0c23ce9558285
Robert Geraghty <rrg053@utulsa.edu>
2020-04-02 13:03:38 -0500

    lotsa changes inorder to get the d3.js working

---
+++ Pages/treedisplay.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace FloChar.Pages
+{
+    public class treedisplayModel : PageModel
+    {
+        [BindProperty]
+        public string jsonData { get; set; }
+
+        public void OnGet()
+        {
+            //System.Diagnostics.Process process = new System.Diagnostics.Process();
+            //System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //startInfo.FileName = "python";
+            //startInfo.Arguments = "wwwroot/lib/dtl_algorithm/DecisionTree.py -f wwwroot/lib/dtl_algorithm/treeData.json";
+            //process.StartInfo = startInfo;
+            //process.Start();
+            //
+            //String command = @"python wwwroot/lib/dtl_algorithm/DecisionTree.py -f wwwroot/lib/dtl_algorithm/test_data.csv -o wwwroot/lib/dtl_algorithm/treeData.json";
+            //ProcessStartInfo cmdsi = new ProcessStartInfo("cmd.exe");
+            //cmdsi.Arguments = command;
+            //Process cmd = Process.Start(cmdsi);
+            //cmd.WaitForExit();
+
+            //string strCmdText;
+            //strCmdText = "wwwroot/lib/dtl_algorithm/DecisionTree.py -f wwwroot/lib/dtl_algorithm/test_data.csv -o wwwroot/lib/dtl_algorithm/treeData.json";
+            //System.Diagnostics.Process.Start("python", strCmdText);
+
+            //jsonData = System.IO.File.ReadAllText(@"Pages/treeData.json");
+        }
+    }
+}
---
+++ wwwroot/lib/dtl_algorithm/js/questionHandler.js
@@ -0,0 +1,11 @@
+﻿function postQuestion() {
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:50337/Question",
+        data: JSON.stringify(document.getElementById("questionValue").value),
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (response) {
+        }
+    });
+}
