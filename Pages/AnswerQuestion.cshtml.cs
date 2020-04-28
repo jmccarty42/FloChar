5c5af342c906683a58fdbd8114a804491f595ff3
John <jmccarty42@gmail.com>
2020-04-28 01:06:31 -0500

    * AnswerQuestion.cshtml: * AnswerQuestion.cshtml.cs: Added Answer Question front end
    * _Layout.cshtml: Added Answer Question to banner

---
+++ Pages/AnswerQuestion.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FloChar.Data;
+using FloChar.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FloChar
+{
+    [Authorize]
+    public class AnswerQuestionModel : PageModel
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public AnswerQuestionModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public List<RootQuestion> RootQuestions { get; set; }
+        public List<SubQuestion> SubQuestions { get; set; }
+
+        public void OnGet()
+        {
+            RootQuestions = _context.RootQuestions.OrderByDescending(x => x.Id).Take(100).ToList();
+            SubQuestions = _context.SubQuestions.OrderByDescending(x => x.Id).Take(100).ToList();
+        }
+    }
+}
